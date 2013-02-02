9005cad88329d260798519b05248551c2243b4b9
Nathan A. Paxton <napaxton@gmail.com>
2013-02-02 10:51:34 -0500

    Add FE model tests to plm test source file

--- tests/plm.R
+++ tests/plm.R
@@ -3,19 +3,20 @@ library(ZeligPanelmodels)
 data(Grunfeld)
 
 z.out.re <- zelig(
-               inv ~ value + capital,
-               data=Grunfeld, 
-               model="pan.plm", 
-               pan.model="random", 
-               pan.index=c("firm","year")
-               )
-# z.out.fe <- zelig(
-#                Formula(inv ~ value + capital),
-#                data=Grunfeld, 
-#                model="pan.plm", 
-#                pan.model="within", 
-#                pan.index=c("firm","year")
-#                )
+    inv ~ value + capital,
+    data=Grunfeld, 
+    model="pan.plm", 
+    pan.model="random", 
+    pan.index=c("firm","year")
+)
+
+z.out.fe <- zelig(
+    Formula(inv ~ value + capital),
+    data=Grunfeld, 
+    model="pan.plm", 
+    pan.model="within", 
+    pan.index=c("firm","year")
+)
 # z.out.fd <- zelig(
 #     inv ~ value + capital,
 #     data=Grunfeld, 
@@ -25,14 +26,20 @@ z.out.re <- zelig(
 #     )
 
 x.out.re <- setx(z.out.re)
+s.out.re <- sim(z.out.re, x=x.out.re)
+s.out.re
+summary(s.out.re)
+
+x.out.fe <- setx(z.out.fe)
+s.out.fe <- sim(z.out.fe, x=x.out.fe)
+s.out.fe
+summary(s.out.fe)
+
 # x.out.l <- setx(z.out, capital=quantile(Grunfeld$capital, 0.25))
 # x.out.h <- setx(z.out, capital=quantile(Grunfeld$capital, 0.75))
 
-x.out.re
 
-
-s.out.re <- sim(z.out.re, x=x.out.re)
 # s.out.fe <-sim(z.out, x=x.out.l, x1=x.out.h)
 
-s.out.re
+
 # s.out.fe
